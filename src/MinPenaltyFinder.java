2517c33bbf73ed54c7ff772748b846d59a40b75f
Christopher Smith <smithcg@ucalgary.ca>
2018-01-17 18:52:29 -0700

    Compleate overhaule of code. Removing object oriented focus.

--- src/MinPenaltyFinder.java
+++ src/MinPenaltyFinder.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MinPenaltyFinder {
 	static int[] taskAssignments = new int[8];							//Order of task assignment from machine 1-8
@@ -10,118 +11,100 @@ public class MinPenaltyFinder {
 		dataArray inputData = new dataArray();
 		Node currentNode = null;
 		
-		int[] bestAssignment = new int[8];								//Stores the current best machine task assignment
-		int lowestPenalty = 1000;												//Stores the lowest penalty found in tree (1000 is test value to see if nothing is found)
+		ArrayList<Integer> bestAssignment = new ArrayList<Integer>(Arrays.asList(-1,-1,-1,-1,-1,-1,-1,-1,-1));	//Stores the current best machine task assignment
+		int lowestPenalty = 1000;										//Stores the lowest penalty found in tree (1000 is test value to see if nothing is found)
 		
+		ArrayList<Integer> remainingMachines = new ArrayList<Integer>(Arrays.asList(0,1,2,3,4,5,6,7));	//Records which machines have not been used in this branch
+		ArrayList<Integer> remainingTasks = new ArrayList<Integer>(Arrays.asList(0,1,2,3,4,5,6,7));						//Records which tasks have not been used in this branch
 		
-		String fileName = "//C:/Users/USER/Desktop/CPSC449Input.txt";		//Input file location and name
-		//String fileName = "//C:/Users/smithcg/Desktop/CPSC449Input.txt";	//Input file location at school
+		ArrayList<Integer> currentPath = new ArrayList<Integer>();		//Records task assignments up to and including this node
+		
+		String fileName = "//C:/Users/USER/Desktop/CPSC449Input.txt";	//Input file location and name
 		FileParser.ReadFile(fileName, inputData);
-		testArray(inputData); 										//Loads file into variables
+		testArray(inputData); 											//Loads file into variables
+				
+		//Create top level MasterNode (machine and task are -1)	
+		currentNode = new Node();
 		
-		//NEED CHECK TO CONFIRM THERE IS AT LEAST ONE MACHINE ASSIGNMENT
+		//Assign forced pairs
+		for(int i=0; i<inputData.forcedPairs.size(); i++)
+		{
+			int[] assignedPair = inputData.forcedPairs.get(i);
+			Node node = new Node(currentNode, assignedPair[0], assignedPair[1], inputData.machinePenalties[assignedPair[0]][assignedPair[1]]);	//Create node for forced pair
+						
+			currentNode = node;											//Move down to new node
+			
+			currentNode.open = false;									//Close node so it won't make new nodes again
+			
+			//FIX THE INDEXING
+			remainingMachines.remove(currentNode.machine);				//Remove machine and task from available list and adds to path
+			remainingTasks.remove(currentNode.task);
+			currentPath.add(currentNode.machine, currentNode.task);
+		}
 		
-		//Create top level MasterNode
-		//Try to use forced assignments first
-		int[][] penalty = inputData.machinePenalties;					//Alternate tag for simple calling
 		
-		if(inputData.forcedPairs.size() != 0){							//If at least one, make it the master
-			int firstForcedMach = inputData.forcedPairs.get(0)[0];
-			int firstForcedTask = inputData.forcedPairs.get(0)[1];
-			
-			System.out.println("Master Machine: " + firstForcedMach + "    Task:" + firstForcedTask);
-			Node masterNode = new Node(firstForcedMach, firstForcedTask, penalty[firstForcedMach][firstForcedTask]);
-			
-			currentNode = masterNode;
-			currentNode.remainingUnassignedTasks = new int[1];			//Prevent other children from being created
-			
-			inputData.forcedPairs.remove(0);							//Remove pair to prevent duplication
-			
-			for(int j=0; j < inputData.forcedPairs.size(); j++){		//If there are more forced, add them, NEED TO CHECK FOR FORBIDDENS
-				int[] assignedPair = inputData.forcedPairs.get(j);
-				System.out.println("New Machine: " + assignedPair[0] + "    Task:" + assignedPair[1]);
-				Node node = new Node(currentNode, assignedPair[0], assignedPair[1], penalty[assignedPair[0]][assignedPair[1]]);
-				currentNode = node;										//Dig down to new node
+		//Create and search remaining tree
+		do																
+		{
+			//Create branches of current machine and/or climbs down
+			while(remainingMachines.size() != 0)								//Check if all possible branches have been created from current node
+			{
+				if(currentNode.open)											//If children have never been created
+				{
+					int nextMachine = remainingMachines.get(0);					//Set the next machine to assign to (implement algorithm to pick best next machine)
+					
+					for(int task=0; task < remainingTasks.size(); task++)			//For each unassigned task
+					{
+						Node node = new Node(currentNode, nextMachine, task, inputData.machinePenalties[nextMachine][task]);	//Create a new node which links to this node
+					}
+				
+					currentNode.open = false;									//Close node so it won't make new nodes again
+				}
+				
+				System.out.print("Remaining machines: " + remainingMachines);
+				System.out.print("Children to check: " + currentNode.children);
+				
+				currentNode = currentNode.children.get(0);					//Pick a node to climb down to
+				remainingMachines.remove(currentNode.machine);				//Remove machine and task from available list and adds to path
+				remainingTasks.remove(currentNode.task);
+				currentPath.add(currentNode.machine, currentNode.task);	
 			}
-		}
-		
-		//Else use 0 as default Master   IS NOT GARANTEED TO BE USABLE SO TO BE FIXED
-		else{
-			Node masterNode = new Node(0, 0, penalty[0][0]);
-			currentNode = masterNode;
-			currentNode.remainingUnassignedTasks = new int[0];		//Prevent other children from being created
-		}
-		
+			
+			//Should always now be in a leaf
+			
+			//Check if best solution
+			if(currentNode.accumulatedPenalty < lowestPenalty){
+				bestAssignment = currentPath;							//Remember best path
+				lowestPenalty = currentNode.accumulatedPenalty;			//Remember lowest penalty
+			}
 
-		//Create remaining tree
-		//Create branches of current machine
-		while(!(currentNode.isMaster && !currentNode.open)){			//Until node is master and master has been closed
-			while(currentNode.remainingMachines.length != 0 && currentNode.remainingUnassignedTasks.length!=0){	//Check if all possible branches have been created from current node (no machines remain)
-				int newMachine = currentNode.remainingMachines[0];		//Set the next machine to assign to as the 0th index
+			//Start climbing up
+			while(currentNode.children.size() == 0)					//Until there is a child to climb down to
+			{	
+				currentNode.parent.children.remove(currentNode);	//Remove child node from parents
 				
-				while(currentNode.remainingUnassignedTasks.length!=0){			//For every remaining unassigned task
-					int newTask = currentNode.remainingUnassignedTasks[0];
-					//System.out.println("Lenght of remaing tasks: " + currentNode.remainingTasks.length);
-					//System.out.println("Lenght of remaing machines: " + currentNode.remainingMachines.length);
-					System.out.println("New Machine: " + newMachine + "    Task:" + currentNode.remainingUnassignedTasks[0]);
-					Node node = new Node(currentNode, newMachine, newTask, penalty[newMachine][newTask]);	//Create a new node which links to this one
-					//System.out.println("Machine: " + currentNode.machine + " has child machine: " + currentNode.children.get(0).machine);
-				}
+				remainingMachines.add(currentNode.machine);			//Re-add machine and task to available list and removes from path
+				remainingTasks.add(currentNode.task);
+				currentPath.add(currentNode.machine, -1);
 				
-				//Dig down one layer and set current node to a child node if possible
-				boolean skip = false;
-				//System.out.println("num of children: " + currentNode.children.size());
-				for(int createdNodeIndex = 0 ; createdNodeIndex < currentNode.children.size(); createdNodeIndex++){	//Check through each child node 
-					if(currentNode.children.get(createdNodeIndex).open && !skip){  									//If child is open and the first found
-						currentNode = currentNode.children.get(createdNodeIndex);									//Set child node as active
-						skip = true;																				//Skip all remaining nodes
-						System.out.println("Current Node dug down to: " + currentNode.machine + "-" + currentNode.task);
-						//System.out.println("Current Node changed to: " + currentNode.machine + "-" + currentNode.task);
-						//System.out.println("New node has: " + currentNode.remainingMachines.length + " remaining machines to test");
-					}
-				}
-				
-				//Check if best solution
-				if(currentNode.remainingMachines.length==0){			//If no machines are left, it must be a leaf
-					if(currentNode.accumulatedPenalty < lowestPenalty){
-						bestAssignment = currentNode.currentPath;		//Assign best path
-						lowestPenalty = currentNode.accumulatedPenalty;	//Assign lowest penalty
-					}
-				}
+				currentNode = currentNode.parent;					//Climb up 
 			}
+			
+		}while(currentNode.parent != null);							//Repeat until node is master again
 		
-			
-			//Climb one layer
-			if(currentNode.parent!=null){								//Guard against null pointer of master node
-				//currentNode.open = false;								//Once you hit the bottom
-				currentNode.parent.children.remove(currentNode);		//Remove child node from checks
-				currentNode = currentNode.parent;						//Close branch and climb one level
-				System.out.println("Current Node moved up to: " + currentNode.machine + "-" + currentNode.task);
-				//System.out.println("Remaining machines to test: " + currentNode.remainingMachines.length);
-			
-				if(currentNode.children.size()!=0){						//If there are any remaining children
-					currentNode = currentNode.children.get(0);			//Dig down to first child
-					System.out.println("Current Node dug down to: " + currentNode.machine + "-" + currentNode.task);
-				}
-			}
-			
-			//Close node if children are closed
-			if(currentNode.children.size()==0){
-				currentNode.open = false;
-			}
-		}
 		
 		
 		
 		
 		//Result
 		System.out.print("Solution: ");
-		for(int i=0 ; i < bestAssignment.length; i++){System.out.print(" " + bestAssignment[i] + " ");}
+		for(int i=0 ; i < bestAssignment.size(); i++){System.out.print(" " + bestAssignment.get(i) + " ");}
 		System.out.print(" Quality: " + lowestPenalty);
 	}
 	
 	
-	//2+2+9+0+2+3+6+2
+
+	
 	
 	
 	
--- src/Node.java
+++ src/Node.java
@@ -2,38 +2,22 @@ import java.util.ArrayList;
 
 public class Node {
 	boolean open = true;
-	boolean isMaster = false;
 	
 	int nodePenalty = 0;										//Records this nodes penalty (may not be needed)
 	int accumulatedPenalty = 0;									//Records total penalty including this node
-	int[] currentPath = new int[8];								//Records task assignments up to and including this node
 	
 	Node parent = null;
 	ArrayList<Node> children = new ArrayList<Node>();
 	
-	int[] remainingMachines = new int[8];
-	int[] remainingTasks = new int[8];
-	int[] remainingUnassignedTasks = new int[8];
-	
-	int machine;
-	int task;
+	int machine = -1;
+	int task = -1;
 
-	Node(int assignedMachine, int assignedTask, int penalty){	//Overload for Master Node
-		isMaster = true;										//Set master
-		
-		machine = assignedMachine;
-		task = assignedTask;
-		
-		int[] options = {0,1,2,3,4,5,6,7};
-		remainingMachines = removeElement(options, assignedMachine);
-		remainingTasks = removeElement(options, assignedTask);		
-		remainingUnassignedTasks = remainingTasks;
-		
-		nodePenalty = penalty;
-		currentPath[assignedMachine] = assignedTask;			//Adds task to task path
-	}
+	
+	//Undefined node used for master
+	Node(){	}	
 	
 	
+	//Regular node
 	Node(Node above, int assignedMachine, int assignedTask, int penalty){							//New branch or leaf node
 		machine = assignedMachine;
 		task = assignedTask;
@@ -41,53 +25,15 @@ public class Node {
 		parent = above;				//Link child to parent
 		parent.addChildNode(this);	//Link parent to child
 		
-		remainingMachines = removeElement(parent.remainingMachines, assignedMachine);	//Remove this machine from available machines
-		remainingTasks = removeElement(parent.remainingTasks, assignedTask);			//Remove this task from available machines
-		remainingUnassignedTasks = remainingTasks;
-		
-		//System.out.print("remainingTasks: ");
-		//for(int i=0 ; i < remainingTasks.length; i++){System.out.print(remainingTasks[i]);}
-		//System.out.println();
-		
-		System.out.println("new penalty: " + penalty);
 		nodePenalty = penalty;
-		accumulatedPenalty = parent.accumulatedPenalty + nodePenalty;
-		System.out.println("accumulatedPenalty: " + accumulatedPenalty);
-		
-		currentPath = parent.currentPath;												//Inherit parent task path
-		currentPath[assignedMachine] = assignedTask;									//Add this node to task path
+		accumulatedPenalty = parent.accumulatedPenalty + nodePenalty;		
 	}
 	
 	//Executed in the parent when a child is created
 	public void addChildNode(Node child){
 		children.add(child);					//Add child parent relationship
+		System.out.println("Parent:" + this.machine + "  was just given child: " + child.machine);
+		System.out.println("Parent:" + this.machine + "  now has # of children: " + this.children.size());
 		
-		//System.out.println("child creation is the problem");
-		//if(parent!=null){System.out.println("Trigrered on parent: " + parent.machine + "    Child: " + this.machine);}
-		
-		this.remainingUnassignedTasks = removeElement(this.remainingUnassignedTasks, child.task);	//Remove this machine from parent search			
-	}
-	
-	//Removes one element
-	public int[] removeElement(int[] oldArray, int toRemove){
-		//System.out.print("Array: ");
-		//for(int i=0 ; i < oldArray.length; i++){System.out.print(oldArray[i]);}
-		//System.out.println("      Removeing: " + toRemove);
-				
-		boolean elementRemoved = false;
-		int[] newArray = new int[oldArray.length-1];
-
-		for(int index=0; index < newArray.length; index++){
-			if(oldArray[index] == toRemove){elementRemoved=true;}	//Wait to find the element to be removed
-			
-			if(!elementRemoved){newArray[index] = oldArray[index];}	//If element has not been removed, map 1 to 1
-			else{newArray[index] = oldArray[index+1];}				//If element has removed, shift all other elements back
-		}
-		
-		//System.out.print("New Array: ");
-		//for(int i=0 ; i < newArray.length; i++){System.out.print(newArray[i]);}
-		//System.out.println();
-		
-		return(newArray);
 	}
 }
